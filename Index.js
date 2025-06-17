cd38295d88adef3cdea8295689b5be696d2872da
mzebitz <markzebitz@gmail.com>
2025-06-17 18:48:13 +0200

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Velkommen til TOPSOE Insight</h1>
+      <p>Her vil du kunne gennemføre en farveprofil-test baseret på Insights Discovery.</p>
+    </main>
+  );
+}
